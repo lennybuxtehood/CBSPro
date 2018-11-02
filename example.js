da493983dfce489e6f5e824c56ad10993066e659
Lennart Nissen <lennartnissen@Lennarts-MacBook-Pro-2.local>
2018-11-02 15:44:12 +0100

    login + redirection

--- example.js
+++ example.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "abc" && password == "123"){
 alert ("Login successfully");
-window.location = "success.html"; // Redirecting to other page.
+window.location = "mainpagee.html"; // Redirecting to other page.
 return false;
 }
 else{
