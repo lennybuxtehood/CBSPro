f965382927af9f67195bf577debedc9d9521a7d9
semo18ab <semo18ab@student.cbs.dk>
2018-11-02 15:33:37 +0100

    C+P

--- example.js
+++ example.js
@@ -1,149 +1,22 @@
-function Login()
-{
-
-    if(empty($_POST['username']))
-    {
-        $this->HandleError("UserName is empty!");
-
-        return false;
-        
-    }
-
-    if(empty($_POST['password']))
-    {var attempt = 3; // Variable to count number of attempts.
-        // Below function Executes on click of login button.
-        function validate(){
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        if ( username == "Formget" && password == "formget#123"){
-        alert ("Login successfully");
-        window.location = "success.html"; // Redirecting to other page.
-        return false;
-        }
-        else{
-        attempt --;// Decrementing by one.
-        alert("You have left "+attempt+" attempt;");
-        // Disabling fields after 3 attempts.
-        if( attempt == 0){
-        document.getElementById("username").disabled = true;
-        document.getElementById("password").disabled = true;
-        document.getElementById("submit").disabled = true;
-        return false;
-        }
-        }
-        }
-
-        $this->HandleError("Password is empty!");
-
-        return false;
-    }
-
-    $username = trim($_POST['username']);
-
-    $password = trim($_POST['password']);
-
-
-    if(!$this->CheckLoginInDB($username,$password))
-    {
-        return false;
-    }
-    session_start();
-    $_SESSION[$this->GetLoginSessionVar()] = $username;
-    return true;
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Formget" && password == "formget#123"){
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+return false;
 }
-
-function CheckLogin()
-{
-
-     session_start();
-
-     $sessionvar = $this->GetLoginSessionVar();
-
-      
-     if(empty($_SESSION[$sessionvar]))
-     {
-        return false;
-     }
-     return true;
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
 }
-
-
-/* Below line is used for online Google font */
-@import url(http://fonts.googleapis.com/css?family=Raleway);
-h2{
-background-color: #FEFFED;
-padding: 30px 35px;
-margin: -10px -50px;
-text-align:center;
-border-radius: 10px 10px 0 0;
 }
-hr{
-margin: 10px -50px;
-border: 0;
-border-top: 1px solid #ccc;
-margin-bottom: 40px;
-}
-div.container{
-width: 900px;
-height: 610px;
-margin:35px auto;
-font-family: 'Raleway', sans-serif;
-}
-div.main{
-width: 300px;
-padding: 10px 50px 25px;
-border: 2px solid gray;
-border-radius: 10px;
-font-family: raleway;
-float:left;
-margin-top:50px;
-}
-input[type=text],input[type=password]{
-width: 100%;
-height: 40px;
-padding: 5px;
-margin-bottom: 25px;
-margin-top: 5px;
-border: 2px solid #ccc;
-color: #4f4f4f;
-font-size: 16px;
-border-radius: 5px;
-}
-label{
-color: #464646;
-text-shadow: 0 1px 0 #fff;
-font-size: 14px;
-font-weight: bold;
-}
-center{
-font-size:32px;
-}
-.note{
-color:red;
-}
-.valid{
-color:green;
-}
-.back{
-text-decoration: none;
-border: 1px solid rgb(0, 143, 255);
-background-color: rgb(0, 214, 255);
-padding: 3px 20px;
-border-radius: 2px;
-color: black;
-}
-input[type=button]{
-font-size: 16px;
-background: linear-gradient(#ffbc00 5%, #ffdd7f 100%);
-border: 1px solid #e5a900;
-color: #4E4D4B;
-font-weight: bold;
-cursor: pointer;
-width: 100%;
-border-radius: 5px;
-padding: 10px 0;
-outline:none;
-}
-input[type=button]:hover{
-background: linear-gradient(#ffdd7f 5%, #ffbc00 100%);
 }
