86a4937e43c71b95ace005a045e42e2ff3fa7c2f
Lennart Nissen <lennartnissen@Lennarts-MacBook-Pro-2.local>
2018-11-28 16:57:40 +0100

    New

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 var attempt = 3; // Variable to count number of attempts.
-// Below function Executes on click of login button.
+// Below function executes on click of login button.
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
