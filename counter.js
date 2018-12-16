f69413924b0f93a1dca5ec3f955ff24045960399
Lennart Nissen <lennartnissen@Lennarts-MacBook-Pro-2.local>
2018-12-16 17:25:04 +0100

    dd

--- counter.js
+++ counter.js
@@ -6,15 +6,19 @@ if (localStorage.getItem("Participants") === null) {
 var memberArr = JSON.parse(localStorage.getItem("Participants"));
 
 document.getElementById("counter").innerHTML = memberArr.length
-document.getElementById("members").innerHTML = "These people are already registered: " + memberArr.toString("")
+document.getElementById("members").innerHTML = "These people are already registered: " + memberArr.toString(", ")
+
+for(i=0; i<memberArr.length; i++){
+    document.getElementById("members").innerHTML += memberArr[i] + ", ";
+}
 
 function register() {
 
     if (memberArr.length < 10) {
         var newMember = document.getElementById("name").value;
-
         memberArr.push(newMember);
         localStorage.setItem("Participants", JSON.stringify(memberArr));
+        //return true;
     } else {
         alert("Course is already full!");
         return false;
